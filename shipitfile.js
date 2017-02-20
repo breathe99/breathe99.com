a20ed2bee7d0036ecb408e84837fe1a403f35396
themezzilla <crollins199@gmail.com>
2017-02-20 13:33:30 -0600

    [cr] started working on shipit deployment file

---
+++ shipitfile.js
@@ -0,0 +1,23 @@
+module.exports = function (shipit) {
+    require('shipit-deploy')(shipit);
+
+    shipit.initConfig({
+        default: {
+            workspace: '/tmp/breathe99',
+            deployTo: '/usr/share/nginx/html',
+            dirToCopy: './',
+            repositoryUrl: 'https://github.com/breathe99/breathe99.com',
+            ignores: ['.git', 'node_modules'],
+            keepReleases: 3,
+            deleteOnRollback: false,
+            key: '~/.ssh/id_rsa.pub',
+            shallowClone: true
+
+        },
+        production: {
+            servers: 'jake@192.241.152.156'
+        }
+
+    });
+
+};
