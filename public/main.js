9d3639f679eaea92c84442ca8b0d9bb4d89a716a
crollins <crollins@drwholdings.com>
2017-03-16 12:18:42 -0500

    [cr] added font imports

--- public/main.js
+++ public/main.js
@@ -1,1 +1,7 @@
 import './styles/base.scss';
+
+const iframe = document.querySelector('iframe');
+
+// iframe.addEventListener('load', function() {
+//   this.closest('.shot-image-sec .logo').setAttribute('style', 'display:none');
+// });
